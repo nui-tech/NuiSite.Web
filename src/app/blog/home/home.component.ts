f3c172b84309aef85f6ce040b3bf594b2ffc2a84
Nui Rattapon <contact@nuirattapon.com>
2017-07-20 18:44:12 +1200

    add blog's home component

---
+++ src/app/blog/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'blog-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+    home;
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
